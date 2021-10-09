1fbaf86bcf8a7ac033fca374aced74791f26b7e0
AlexxCourter <alex.courter@gmail.com>
2021-10-09 11:14:45 -0400

    added another hello

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jerald");
+
+        //addition by Alex Courter
+        System.out.println("Hello Jerald, my name is Alex");
+
     }
 }
